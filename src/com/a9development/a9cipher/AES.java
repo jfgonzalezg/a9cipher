1efb021ba848710149b73982bfb7e9342331f35d
Daniel Kotowski <Daniel Kotowski@localhost>
2010-07-07 15:49:30 -0400

    Added comment to notify that AES is just a wrapper for Rijndael

--- src/com/a9development/a9cipher/AES.java
+++ src/com/a9development/a9cipher/AES.java
@@ -1,6 +1,25 @@
+//    Copyright 2010 Daniel James Kotowski
+//
+//    This file is part of A9Cipher.
+//
+//    A9Cipher is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    A9Cipher is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with A9Cipher.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.a9development.a9cipher;
 
-// A wrapper around the RijndaelCipher class that lets it be used as AESCipher as well.
+// A wrapper around the RijndaelCipher class that lets it be used as AESCipher
+// as well. This provides no new functionality other than to allow the
+// RijndaelCipher class to be called something else.
 public class AES {
 	private Rijndael rd;
 	
