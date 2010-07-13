b9ecf41ebd361ab38cbdbc5cc0878d2f63edc8ad
Daniel Kotowski <Daniel Kotowski@localhost>
2010-07-13 05:08:09 -0400

    Renamed some methods

--- src/com/a9development/ciphertestharness/A9Utility.java
+++ src/com/a9development/ciphertestharness/A9Utility.java
@@ -2,7 +2,7 @@ package com.a9development.ciphertestharness;
 
 public class A9Utility {
 
-	public static String bytesToString(byte[] B) {
+	public static String bytesToHex(byte[] B) {
 		String resultString = "";
 		for (int i = 0; i < B.length; i++) {
 			resultString += Integer.toString((B[i] & 0xff) + 0x100, 16).substring(1);
@@ -10,7 +10,7 @@ public class A9Utility {
 		return resultString;
 	}
 	
-	public static byte[] stringToBytes(String s) {
+	public static byte[] hexToBytes(String s) {
 		byte[] bytes = new byte[s.length() / 2];
 		for (int i = 0; i < s.length(); i+=2) {
 			bytes[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
