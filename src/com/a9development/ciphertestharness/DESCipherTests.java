1267f2e580e3f48d22618b6e64705efc26a65a17
agrajag9 <agrajag9@localhost>
2010-07-21 14:51:34 -0400

    Updated tests to use the new com.a9development.cipher package

--- src/com/a9development/ciphertestharness/DESCipherTests.java
+++ src/com/a9development/ciphertestharness/DESCipherTests.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.a9development.a9cipher.A9Utility;
-import com.a9development.a9cipher.DESCipher;
+import com.a9development.cipher.A9Utility;
+import com.a9development.cipher.DESCipher;
 
 public class DESCipherTests {
 
