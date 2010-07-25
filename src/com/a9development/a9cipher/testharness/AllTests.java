36e7cd9a28f14f10edc55e0427e4a008212296a8
Daniel Kotowski <Daniel Kotowski@localhost>
2010-07-25 18:47:14 -0400

    Added tests for RC4Cipher

--- src/com/a9development/a9cipher/testharness/AllTests.java
+++ src/com/a9development/a9cipher/testharness/AllTests.java
@@ -12,7 +12,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { A9UtilityTests.class, DESCipherTests.class, RijndaelCipherTests.class, SHA1Tests.class, SHA2Tests.class } )
+@Suite.SuiteClasses( { A9UtilityTests.class, DESCipherTests.class, RijndaelCipherTests.class, RC4CipherTests.class, SHA1Tests.class, SHA2Tests.class } )
 public class AllTests {
 
 	public static Test suite() {
