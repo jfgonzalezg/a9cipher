5b1aed88c4efa18bf2d2791d9b19b25edc7b314e
agrajag9 <agrajag9@localhost>
2010-07-27 13:13:38 -0400

    Should have been with last commit (Added Radix64)

--- src/com/a9development/cipher/testharness/AllTests.java
+++ src/com/a9development/cipher/testharness/AllTests.java
@@ -12,7 +12,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { A9UtilityTests.class, DESCipherTests.class, RijndaelCipherTests.class } )
+@Suite.SuiteClasses( { A9UtilityTests.class, DESCipherTests.class, RijndaelCipherTests.class, Radix64Tests.class } )
 public class AllTests {
 
 	public static Test suite() {
