fc89b6e3e0e1da1074b10acd27eb036d1ca511c6
Daniel Kotowski <Daniel Kotowski@localhost>
2010-08-26 20:39:38 -0400

    Updated all files to contain necessary license header

--- src/com/a9development/a9cipher/testharness/AllTests.java
+++ src/com/a9development/a9cipher/testharness/AllTests.java
@@ -1,3 +1,20 @@
+//    Copyright 2010 Daniel James Kotowski
+//
+//    This file is part of A9Cipher.
+//
+//    A9Cipher is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    A9Cipher is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with A9Cipher.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.a9development.a9cipher.testharness;
 
 import org.junit.runner.RunWith;
