a36a694cf6ef9dc467ab0083633479b3b065ce74
Daniel Kotowski <Daniel Kotowski@localhost>
2010-10-21 19:06:51 -0400

    Added javadocs to A9Utility

--- src/com/a9development/A9Utility.java
+++ src/com/a9development/A9Utility.java
@@ -3,8 +3,8 @@
 //    This file is part of A9Utilities.
 //
 //    A9Cipher is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Lesser General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
+//    it under the terms of the GNU Lesser General Public License as published
+//	  by the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
 //    A9Cipher is distributed in the hope that it will be useful,
@@ -17,8 +17,23 @@
 
 package com.a9development;
 
+/**
+ * A utility class used in many A9Development projects. All arrays are
+ * big-endian (most significant bit first).
+ * 
+ * TODO: Add error catching to make sure all arrays are the right size
+ * 
+ * @author Daniel Kotowski
+ * @version 0.1
+ *
+ */
 public class A9Utility {
 
+	/**
+	 * Converts a byte array to a representative string of hex digits.
+	 * @param b byte array to convert
+	 * @return representative string of hex digits
+	 */
 	public static String bytesToHex(byte[] b) {
 		String s = "";
 		for (int i = 0; i < b.length; i++) {
@@ -30,14 +45,25 @@ public class A9Utility {
 		return s;
 	}
 
+	/**
+	 * Converts a string of hex digits to a representative byte array
+	 * @param s string of hex digits to convert
+	 * @return representative byte array
+	 */
 	public static byte[] hexToBytes(String s) {
 		byte[] b = new byte[s.length() / 2];
 		for (int i = 0; i < s.length(); i+=2) {
-			b[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+			b[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) 
+					+ Character.digit(s.charAt(i+1), 16));
 		}
 		return b;
 	}
 
+	/**
+	 * Converts an int to a representative byte array
+	 * @param i int to convert
+	 * @return representative byte array
+	 */
 	public static byte[] intToBytes(int i) {
 		byte[] b = new byte[4];
 		for (int c = 0; c < 4; c++) {
@@ -46,10 +72,20 @@ public class A9Utility {
 		return b;
 	}
 
+	/**
+	 * Converts a byte array to a representative int
+	 * @param b byte array to convert
+	 * @return representative int
+	 */
 	public static int bytesToInt(byte[] b) {
 		return ((b[0] << 24) & 0xff000000) | ((b[1] << 16) & 0xff0000) | ((b[2] << 8) & 0xff00) | (b[3] & 0xff);
 	}
 
+	/**
+	 * Converts a long to a representative byte array
+	 * @param l long to convert
+	 * @return representative byte array
+	 */
 	public static byte[] longToBytes(long l) {
 		byte[] b = new byte[8];
 		for (int c = 0; c < 8; c++) {
@@ -58,6 +94,11 @@ public class A9Utility {
 		return b;
 	}
 
+	/**
+	 * Converts a byte to a binary array of booleans
+	 * @param b byte to convert
+	 * @return representative binary array
+	 */
 	public static boolean[] byteToBits(byte b) {
 		boolean[] bits = new boolean[8];
 		for (int i = 0; i < 8; i++) {
@@ -66,6 +107,11 @@ public class A9Utility {
 		return bits;
 	}
 
+	/**
+	 * Converts a binary array of booleans to an 8-byte array
+	 * @param bits binary array to convert
+	 * @return representative 8-byte array
+	 */
 	public static byte[] bitsTo8Bytes(boolean[] bits) {
 		byte[] b = new byte[8];
 		for (int i = 0; i < 8; i++) {
@@ -77,21 +123,45 @@ public class A9Utility {
 		return b;
 	}
 
+	/**
+	 * Gets the i'th bit of int x
+	 * @param x int to get bit from
+	 * @param i offset
+	 * @return the i'th bit of x
+	 */
 	public static int getBit(int x, int i) {
 		return (x >>> i) & 0x01;
 	}
 
+	/**
+	 * Gets the i'th bit of int array x 
+	 * @param x int to get bit from
+	 * @param i offset
+	 * @return 
+	 */
 	public static int getBit(int[] x, int i) {
 		return (x[i / 32] >>> (i % 32)) & 0x01;
 	}
 	
+	/**
+	 * Used to set or clear a bit in an int array
+	 * @param x int array
+	 * @param i offset
+	 * @param v if odd, set; else, clear
+	 */
 	public static void setBit(int[] x, int i, int v) {
 		if ((v & 0x01) == 1)
-			x[i / 32] |= 1 << (i % 32); // set it
+			x[i / 32] |= 1 << (i % 32); // set bit
 		else
-			x[i / 32] &= ~(1 << (i % 32)); // clear it
+			x[i / 32] &= ~(1 << (i % 32)); // clear bit
 	}
 	
+	/**
+	 * Gets the i'th 4-bit nibble from int x
+	 * @param x int to get 4-bit nibble from
+	 * @param i 4-bit offset
+	 * @return 4-bit nibble
+	 */
 	public static int getNibble(int x, int i) {
 		return (x >>> (4 * i)) & 0x0F;
 	}
